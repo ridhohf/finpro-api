375142c8ed7c160a6f4612f49d14cd18e90f6ffa
Tambuahc1ek <133321330+Tambuahc1ek@users.noreply.github.com>
2025-10-09 16:10:07 +0700

    fix: error

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -324,7 +324,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -325,7 +325,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
